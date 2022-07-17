017bf66c88728cdc1a88501d4c8cb0132b1d60fd
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-07-17 17:19:29 +0200

    contacts form

--- src/components/PhoneBookForm/PhoneBookForm.jsx
+++ src/components/PhoneBookForm/PhoneBookForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useCreateContactMutation } from "../../redux/contactSlice";
 import { Spinner } from "../spinner";
 import { Form, Label, Input, SubmitButton } from "./PhoneBookForm.styled";
@@ -5,13 +6,31 @@ import { Form, Label, Input, SubmitButton } from "./PhoneBookForm.styled";
 export const PhoneBookForm = () => {
     const [createContact, {isLoading}] = useCreateContactMutation();
 
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    const handleChange = event => {
+
+    const {name, value} = event.target;
     
+    switch(name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default: return;
+    };
+  };
 
     const handleSubmit = e => {
         e.preventDefault();
 
-        const name = e.currentTarget.elements.name.value;
-        const number = e.currentTarget.elements.number.value;
+        /* const name = e.currentTarget.elements.name.value;
+        const number = e.currentTarget.elements.number.value; */
 
         const contact = {
           name,
@@ -26,9 +45,25 @@ export const PhoneBookForm = () => {
         <>
           <Form autoComplete="off" onSubmit={handleSubmit}>
             <Label>Name</Label>
-               <Input type="text" name="name"/>
+               <Input
+               type="text"
+               name="name"
+               value={name}
+               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+               required
+               onChange={handleChange}/>
+            
             <Label>Number</Label>
-               <Input type="text" name="number"/>
+               <Input
+                type="number"
+                name="number"
+                value={number}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                onChange={handleChange}/>
+
             <SubmitButton type="submit" disabled={isLoading}>
                 {isLoading && <Spinner size={10} />}
                 Create contact
